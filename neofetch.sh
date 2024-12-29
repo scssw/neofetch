28c35403d79e163a708d78785793676bb0cf3791
scssw <90193529+scssw@users.noreply.github.com>
2024-12-29 10:19:48 +0800

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -4,7 +4,133 @@
 curl -sSL https://raw.githubusercontent.com/scssw/neofetch/refs/heads/master/install_neofetch.sh | bash
 
 # 更新 .bashrc 文件，使得每次登录时显示 neofetch
-echo 'if [ -x "$(command -v neofetch)" ]; then neofetch; fi' >> ~/.bashrc
+#!/bin/bash
+
+# 定义要添加的完整内容
+FULL_BASHRC_CONTENT=$(cat <<'EOF'
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+#if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+#    . /etc/bash_completion
+#fi
+. "/root/.acme.sh/acme.sh.env"
+EOF
+)
+
+# 定义要添加的 neofetch 检查代码
+NEOFETCH_CHECK='if [ -x "$(command -v neofetch)" ]; then neofetch; fi'
+
+# 检查 .bashrc 文件是否存在
+BASHRC_FILE="$HOME/.bashrc"
+
+if [ ! -f "$BASHRC_FILE" ]; then
+    # 如果 .bashrc 文件不存在，创建并写入完整内容
+    echo "$FULL_BASHRC_CONTENT" > "$BASHRC_FILE"
+    echo "$NEOFETCH_CHECK" >> "$BASHRC_FILE"
+else
+    # 如果 .bashrc 文件存在，检查是否已包含内容
+    if ! grep -q "$NEOFETCH_CHECK" "$BASHRC_FILE"; then
+        # 如果未包含 neofetch 检查代码，则添加
+        echo "$NEOFETCH_CHECK" >> "$BASHRC_FILE"
+    fi
+fi
+
+echo ".bashrc 更新完成。"
+
 
 # 使 .bashrc 的更改生效
 source ~/.bashrc
